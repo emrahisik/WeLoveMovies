dd5eb53b9a0ecd242854546ea07c20466767f538
Emrah Isik <emrahisk@gmail.com>
2021-10-19 10:27:15 -0400

    read movieId and connected theaters and reviews added

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,5 +1,20 @@
 const service = require('./movies.service');
-const asyncErrorBoundary = require('../errors/asyncErrorBoundary')
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+const mapProperties = require('../utils/map-properties');
+
+const movieIdExists = async (req, res, next) => {
+    const { movieId } = req.params;
+    const data = await service.read(movieId);
+    if(!data){
+        next({
+            status: 404,
+            message: `The movie id ${movieId} not found!`
+        });
+    }else{
+        res.locals.data = data;
+        next();
+    };
+};
 
 const list = async (req,res,next) => {
     const { is_showing } = req.query;
@@ -7,11 +22,38 @@ const list = async (req,res,next) => {
     if(!is_showing){
         movies = await service.list();
     }else{
-        movies = await service.qList(is_showing);
+        movies = await service.listQuery(is_showing);
     }
     res.json({data: movies})
 };
 
+const read = (req, res, next) => {
+    const { data } = res.locals;
+    res.json({ data })
+};
+
+const readTheaters = async (req, res, next) => {
+    const data = await service.readTheaters(req.params.movieId);
+    res.json({data});
+};
+
+const readReviews = async (req, res, next) => {
+    const data = await service.readReviews(req.params.movieId);
+    const criticsConfig = {
+        critic_id: "critic.critic_id",
+        preferred_name: "critic.preferred_name",
+        surname: "critic.surname",
+        organization_name: "critic.organization_name",
+        //created_at: "critic.created_at",
+        //updated_at: "critic.updated_at"
+    }
+    const addCategory = data.map(el => mapProperties(criticsConfig)(el))
+    res.json({data: addCategory});
+};
+
 module.exports = {
     list: asyncErrorBoundary(list),
+    read: [asyncErrorBoundary(movieIdExists), read],
+    readTheaters: [asyncErrorBoundary(movieIdExists), readTheaters],
+    readReviews: [asyncErrorBoundary(movieIdExists), readReviews]
 };
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -5,7 +5,7 @@ const list = (query) => {
                 .select('*');
                 
 };
-const qList = (value) => {
+const listQuery = (value) => {
     return knex('movies as m')
                 .leftJoin('movies_theaters as mt', 'mt.movie_id', 'm.movie_id')
                 .select('m.*')
@@ -15,7 +15,31 @@ const qList = (value) => {
                 
 };
 
+const read = (movie_id) => {
+    return knex('movies')
+            .select('*')
+            .where({movie_id})
+            .first();
+};
+
+const readTheaters = (movie_id) => {
+    return knex('movies_theaters as mt')
+            .join('theaters as t', 't.theater_id', 'mt.theater_id')
+            .select("t.*",'mt.*')
+            .where({ movie_id })
+};
+
+const readReviews = (movie_id) => {
+    return knex('reviews as r')
+            .join('critics as c', 'r.critic_id', 'c.critic_id')
+            .select("r.*",'c.*')
+            .where({ movie_id })
+};
+
 module.exports ={
     list,
-    qList,
+    listQuery,
+    read,
+    readTheaters,
+    readReviews,
 }
