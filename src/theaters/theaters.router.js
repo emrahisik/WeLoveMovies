1e0b81733eca446f1c2262cd0d3a497bc5484aed
Emrah Isik <emrahisk@gmail.com>
2021-10-21 15:02:12 -0400

    methodNotAllowed added

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('./theaters.controller.js');
+const methodNotAllowed = require('../errors/methodNotAllowed')
 
-router.route('/').get(controller.list)
+router.route('/').get(controller.list).all(methodNotAllowed)
 
 module.exports = router;
